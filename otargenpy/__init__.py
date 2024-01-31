a60d023368b50aef9739d544e44e70787bcfc3be
Amir Feizi <amir.feizi@omass.com>
2024-01-31 12:19:54 +0000

    Bump version: 0.1.0 → 0.1.1

--- otargenpy/__init__.py
+++ otargenpy/__init__.py
@@ -1,5 +1,5 @@
 """
-Accessing tidy data from Open Target Genetics for Python
+Accessing tidy data from Open Target Genetics GraphQL API for Python
 =======================
 otargenpy interact seamlessly with Open Target Genetics' GraphQL endpoint 
 to query and retrieve tidy data tables, facilitating the analysis of genetic data.
@@ -9,6 +9,6 @@ API(<https://genetics.opentargets.org/api>).
 
 __author__ = """Amir Feizi"""
 __email__ = "afeizi@gmail.com"
-__version__ = "0.1.0"
+__version__ = '0.1.1'
 
 from . import query, utils
