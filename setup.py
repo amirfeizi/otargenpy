06a4fbae68687ca3a4b2df15e3a4c505bfc4ae28
Amir Feizi <amir.feizi@omass.com>
2024-02-06 13:25:44 +0000

    finalizing the version 0.1.4

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/amirfeizi/otargenpy/',
-    version='0.1.1',
+    version='0.1.4',
     zip_safe=False,
 )
